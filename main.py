e076b148cc0e90f2e4ce49d4ee32dac8aad8e1ce
Reinan Kauã <69223145+reinankaua@users.noreply.github.com>
2024-08-29 21:11:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from routes import init_routes
+
+app = FastAPI()
+
+@app.get("/ola")
+def say_hello():
+    return "Hello"
+
+init_routes(app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
